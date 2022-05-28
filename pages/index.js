32c0463eb3ef56e9191a55c9fd1974f8194649fa
Kyle <kmarunda@gmail.com>
2022-05-28 11:14:58 +0200

    final

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
-
+import Script from "next/script";
 export default function Home() {
   return (
     <Layout home>
